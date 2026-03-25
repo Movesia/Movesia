21f11321df6614379e2cf5c01c56e12f87a7fe73
Hannyel <hannyeljimenez@gmail.com>
2026-03-24 22:33:26 -0400

    owner change

--- forge.config.ts
+++ forge.config.ts
@@ -23,9 +23,9 @@ const rootDir = process.cwd();
  */
 const nativeModules = [
   'better-sqlite3',
-  'bindings',           // dependency of better-sqlite3 (finds .node files)
-  'file-uri-to-path',   // dependency of bindings
-  'prebuild-install',   // dependency of better-sqlite3
+  'bindings', // dependency of better-sqlite3 (finds .node files)
+  'file-uri-to-path', // dependency of bindings
+  'prebuild-install', // dependency of better-sqlite3
 ];
 
 const config: ForgeConfig = {
@@ -85,7 +85,7 @@ const config: ForgeConfig = {
   ],
   publishers: [
     new PublisherGithub({
-      repository: { owner: 'Hannyel0', name: 'Movesia' },
+      repository: { owner: 'Movesia', name: 'Movesia' },
       prerelease: true,
       draft: true,
     }),
@@ -99,19 +99,19 @@ const config: ForgeConfig = {
         {
           // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
           entry: 'src/main.ts',
-          config: 'config/vite.main.config.ts'
+          config: 'config/vite.main.config.ts',
         },
         {
           entry: 'src/preload.ts',
-          config: 'config/vite.preload.config.ts'
-        }
+          config: 'config/vite.preload.config.ts',
+        },
       ],
       renderer: [
         {
           name: 'main_window',
-          config: 'config/vite.renderer.config.ts'
-        }
-      ]
+          config: 'config/vite.renderer.config.ts',
+        },
+      ],
     }),
     // Fuses are used to enable/disable various Electron functionality
     // at package time, before code signing the application
@@ -122,9 +122,9 @@ const config: ForgeConfig = {
       [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
       [FuseV1Options.EnableNodeCliInspectArguments]: false,
       [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-      [FuseV1Options.OnlyLoadAppFromAsar]: true
-    })
-  ]
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
 };
 
 export default config;
